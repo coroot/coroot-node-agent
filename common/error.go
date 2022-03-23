ef68f139abcd9e8e8f681b27a51733de8e20afec
Nikolay Sivko <n.sivko@gmail.com>
2022-03-23 13:56:59 +0300

    fix logging suppression

--- common/error.go
+++ common/error.go
@@ -3,5 +3,5 @@ package common
 import "strings"
 
 func IsNotExist(err error) bool {
-	return strings.Contains(err.Error(), "no such file or directory") && strings.Contains(err.Error(), "no such process")
+	return strings.Contains(err.Error(), "no such file or directory") || strings.Contains(err.Error(), "no such process")
 }
