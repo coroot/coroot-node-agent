abc9e3cac8041818f00550c735e113a4a2a6b633
Nikolay Sivko <n.sivko@gmail.com>
2023-07-27 17:49:21 +0300

    fix TCP connection timestamp handling in the case of using non-blocking connect

--- ebpftracer/ebpf/tcp/state.c
+++ ebpftracer/ebpf/tcp/state.c
@@ -163,10 +163,6 @@ int sys_enter_connect(void *ctx) {
     }
     __u64 id = bpf_get_current_pid_tgid();
     bpf_map_update_elem(&fd_by_pid_tgid, &id, &args.fd, BPF_ANY);
-    struct sk_info k = {};
-    k.pid = id >> 32;
-    k.fd = args.fd;
-    bpf_map_delete_elem(&connection_timestamps, &k);
     return 0;
 }
 
