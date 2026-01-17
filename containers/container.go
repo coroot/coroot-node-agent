54597e65e72d304891c5b3c00ff603b7c7a6a5b4
Eugene Bykov <44170496+ML-ZoneReaper@users.noreply.github.com>
2026-01-17 20:48:55 +0700

    Update container.go

--- containers/container.go
+++ containers/container.go
@@ -23,6 +23,7 @@ import (
 	"golang.org/x/exp/maps"
 	"inet.af/netaddr"
 	"k8s.io/klog/v2"
+	v1 "k8s.io/api/core/v1"
 )
 
 var (
@@ -216,23 +217,31 @@ func NewContainer(id ContainerID, cg *cgroup.Cgroup, md *ContainerMetadata, pid 
 }
 
 func GetPrimaryContainer(pod *v1.Pod) string {
-    if defaultContainer, exists := pod.Annotations["kubectl.kubernetes.io/default-container"]; exists {
-        for _, c := range pod.Spec.Containers {
-            if c.Name == defaultContainer { return defaultContainer }
-        }
-    }
-    
-    if appName, exists := pod.Labels["app.kubernetes.io/name"]; exists {
-        for _, c := range pod.Spec.Containers {
-            if c.Name == appName { return appName }
-        }
-    }
-    
-    if len(pod.Spec.Containers) > 0 {
-        return pod.Spec.Containers[0].Name
-    }
-    
-    return ""
+	if pod == nil {
+		return ""
+	}
+
+	if defaultContainer, exists := pod.Annotations["kubectl.kubernetes.io/default-container"]; exists && defaultContainer != "" {
+		for _, container := range pod.Spec.Containers {
+			if container.Name == defaultContainer {
+				return defaultContainer
+			}
+		}
+	}
+
+	if appName, exists := pod.Labels["app.kubernetes.io/name"]; exists && appName != "" {
+		for _, container := range pod.Spec.Containers {
+			if container.Name == appName {
+				return appName
+			}
+		}
+	}
+
+	if len(pod.Spec.Containers) > 0 {
+		return pod.Spec.Containers[0].Name
+	}
+
+	return ""
 }
 
 func (c *Container) Close() {
